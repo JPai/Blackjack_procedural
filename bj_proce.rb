4a3a4d687fcd4a6570bd645bb3bc843b153c6880
Justin Pai <justin.pai@gmail.com>
2014-06-21 17:02:33 +0800

    modified bj game and upload the tic_tac_toe game

--- bj_proce.rb
+++ bj_proce.rb
@@ -2,9 +2,13 @@
 
 #This is a procedural blackjack game built for:
 #Tealeaf course - Introduction to Ruby and Web Development - Lesson 1
+#Modify the code by implementing more methods
 #
 #Authur: Justin Pai
-#Date: June 05 2014
+#Date: June 09 2014
+#Rev: 0.2
+
+require 'pry'
 
 BJ = 100 #When it's BlackJack, the total value will retrun -1
 BUSTED = -2 #When total value is over 21
@@ -19,12 +23,18 @@ cards = [['Spade', 'A'], ['Spade', 2], ['Spade', 3], ['Spade', 4], ['Spade', 5],
       ['Club', 'A'], ['Club', 2], ['Club', 3], ['Club', 4], ['Club', 5], ['Club', 6], ['Club', 7], 
       ['Club', 8], ['Club', 9], ['Club', 10], ['Club', 'J'],['Club', 'Q'],['Club', 'K']]
 
+
+#adding "=>" in front of prompt
+def say(msg)
+  puts "=> #{msg}"
+end
+
 #playing or not
 playing = true
 
 #Asking the player's name
-puts "Welcome to our Blackjack Game!"
-puts "What's your name?"
+say "Welcome to our Blackjack Game!"
+say "What's your name?"
 player_name = gets.chomp.strip
 
 #Data structure for the player and delaer
@@ -37,66 +47,92 @@ def hit(c_cards)
   c_cards.pop
 end
 
+#convert suits into 10
+#"arr" has the data type of the card array [[suit1, number1], [suit2, number2],...]
+#return an array as the value of all cards [number1, number2,...]
+def convert_suits (arr)
+  arr.map do |a_v|
+    a_v[1] == 'J' || a_v[1] == 'Q' || a_v[1] == 'K' ? 10 : a_v[1]
+    #NOTE: array in array is pass by reference
+  end 
+end 
+
+#convert the first A into 11
+#"arr" is the value of all cards [number1, number2,...]
+#return an array as the value of all cards [number1, number2,...]
+def convert_a (arr)
+  first_A = true #check if it's the first A => in this case, A will be 11
+  #convert A into 1 or 11
+  arr.map! do |v|
+    if v == 'A'
+      if first_A 
+        first_A = false
+        v = 11          
+      else
+        v = 1
+      end   
+    else
+      v
+    end
+  end
+end
+
+#return sum with a (substract 10 if busted)
+def sum_with_a(arr)
+  sum = 0
+  arr.each {|v| sum += v}
+  if busted?(sum)
+    sum -= 10 #convert first A from 11 to 1
+    busted?(sum) ? sum = BUSTED : sum
+  end
+  sum
+end
+
+
+#check if it's busted
+def busted?(sum)
+  sum > 21 ? true : false
+end
+
+#check if it's blackjack
+def blackjack?(arr)
+  arr == ['A', 10] || arr == [10, 'A'] ? true : false
+end
+
+
 #return the total value of the c_arr[]
 def total(c_arr)	
   sum = 0 #sum of the values
-	
+
+
   #convert J, Q, K into 10
-  n_c_arr = c_arr.map do |a_v|
-    if a_v[1] == 'J' || a_v[1] == 'Q' || a_v[1] == 'K'
-      10 #NOTE: array is reference by pointer!
-    else
-      a_v[1]
-    end
-  end
+  n_c_arr = convert_suits c_arr
+
+  
+#binding.pry
+
   #check if A exists
   if !n_c_arr.include?('A')
     n_c_arr.each {|v| sum += v}
   #BUSTED situation
-    if sum > 21
-      sum = BUSTED
-    else
-      sum
-    end
+    busted?(sum) ? sum = BUSTED : sum
   #BJ situation
-  elsif n_c_arr == ['A', 10] || n_c_arr == [10, 'A']
+  elsif blackjack?(n_c_arr)
     sum = BJ
   else
-    first_A = true #check if it's the first A => in this case, A will be 11
-    #convert A into 1 or 11
-    n_c_arr.map! do |v|
-      if v == 'A'
-        if first_A 
-          first_A = false
-          v = 11					
-        else
-          v = 1
-        end		
-      else
-        v
-      end
-    end
+    convert_a(n_c_arr)
   #calculate sum now
-    n_c_arr.each {|v| sum += v}
-    if sum > 21
-      sum -= 10 #convert first A from 11 to 1
-      if sum > 21
-        sum = BUSTED
-      end
-    else
-      sum
-    end
-    sum
+    sum_with_a(n_c_arr)  
   end
 end
 
 #Asking the player to hit or stay.
 #Return true for hit, and false for stay
 def hit?
-  puts "'Hit' or 'Stay'?"
+  say "'Hit' or 'Stay'?"
   ans = gets.chomp.strip.upcase
   while ans != 'HIT' && ans != 'STAY'
-    puts "Note: enter 'Hit' or 'Stay'"
+    say "Note: enter 'Hit' or 'Stay'"
     ans = gets.chomp.strip.upcase
   end
   if ans == 'HIT'
@@ -107,14 +143,14 @@ def hit?
 end
 
 #Display the current card status for the player and dealer
-#Show dealer's cards if d_s is true
+#Show dealer's cards if d_s is true; show only 1 cards of the dealer if false
 def show_cards_status(player, dealer, d_s)
   if d_s
-    puts "#{dealer[:name]} cards: " + dealer[:card_get].to_s
-    puts "#{player[:name]} cards: " + player[:card_get].to_s
+    say "#{dealer[:name]} cards: #{dealer[:card_get]}"
+    say "#{player[:name]} cards: #{player[:card_get]}"
   else
-    puts "#{dealer[:name]} cards: [[?, ?], [?, ?]]"
-    puts	"#{player[:name]} cards: " + player[:card_get].to_s
+    say "#{dealer[:name]} cards: [[?, ?], #{dealer[:card_get][1]}]"
+    say	"#{player[:name]} cards: #{player[:card_get]}"
   end
 end
 
@@ -145,20 +181,21 @@ while playing
       show_cards_status(player, dealer, false)
       if player_total == BUSTED
         #print out some info
-        puts "#{player[:name]}: Oh No! Busted!"
+        say "#{player[:name]}: Oh No! Busted!"
         break
       end
       if player_total == 21
         #print out some info
-        puts "#{player[:name]}: Great! Got 21!"
+        say "#{player[:name]}: Great! Got 21!"
         break
       end
     end
   else
-    puts "#{player[:name]}: Awesome! Blackjack!"
+    say "#{player[:name]}: Awesome! Blackjack!"
   end
 
 	#dealer's move
+  show_cards_status(player, dealer, true)
   if dealer_total != BJ
     #dealer needs to be over 17
     while dealer_total < 17 
@@ -166,7 +203,7 @@ while playing
       dealer_total = total(dealer[:card_get])
       show_cards_status(player, dealer, true)
       if dealer_total == BUSTED
-        puts "Dealer: busted!!"
+        say "Dealer: busted!!"
         break
       end
       if dealer_total == 21
@@ -174,23 +211,23 @@ while playing
       end
     end
   else
-    puts "Dealer: blackjack!!"
+    say "Dealer: blackjack!!"
   end
 
   #Game results
   if player_total > dealer_total
-    puts "Congradulations! You win!"
+    say "Congradulations! You win!"
   elsif player_total == dealer_total
-    puts "It's a push. No one wins."
+    say "It's a push. No one wins."
   else
-    puts "Ohoh... Dealer wins!"
+    say "Ohoh... Dealer wins!"
   end
 
 #Asking player if he wants to play again
-  puts "Do you want to play again? (Yes/No)"
+  say "Do you want to play again? (Yes/No)"
   ans = gets.chomp.strip.upcase
   while ans != 'YES' && ans != 'NO'
-    puts "Note: enter 'YES' or 'NO'"
+    say "Note: enter 'YES' or 'NO'"
     ans = gets.chomp.strip.upcase
   end
   if ans == 'YES'
---
+++ tic_tac_toe.rb
@@ -0,0 +1,214 @@
+#tic_tac_toe.rb
+
+#Exercise for Ruby - build a tic_tac_toe game
+#Tealeaf - Intro - lesson 1
+#
+#Author: Justin Pai
+#Date: June 6 14
+#Ver: 0.1
+
+require 'pry'
+
+NO_ONE_S = 0 #the block is not selected
+PLAY_S = 1 #the block selected by player
+COM_S = -1 #the block selected by computer
+
+
+NO_ONE_WIN = 0 #no one wins the game
+PLAYER_WIN = 1 #the play wins
+COM_WIN = 2 #the computer wins
+TIE_GAME = 3 #no one wins and all blocks are taken!
+
+#adding "=>" in front of prompt
+def say(msg)
+  puts "=> #{msg}"
+end
+
+#return a new board
+def new_board
+#the board array, true: the block is taken; false: the block is not taken
+#the order for the array value is referring to the block location as following:
+#[r0-c0, r0-c1, r0-c2, r1-c0, r1-c1, r1-c2, r2-c0, r2-c1, r2-c2]
+board_array = [NO_ONE_WIN, NO_ONE_WIN, NO_ONE_WIN, NO_ONE_WIN, NO_ONE_WIN, 
+  NO_ONE_WIN, NO_ONE_WIN, NO_ONE_WIN, NO_ONE_WIN]
+end
+
+
+#givin an location array [r, c], return this location to the board_array order
+#example: if location array  = [0, 0], return number 1 => 
+def block_loc_convert(location)
+  case 
+  when location == [0, 0]
+    0
+  when location == [0, 1]
+    1
+  when location == [0, 2]
+    2
+  when location == [1, 0]
+    3
+  when location == [1, 1]
+    4
+  when location == [1, 2]
+    5
+  when location == [2, 0]
+    6
+  when location == [2, 1]
+    7
+  when location == [2, 2]
+    8
+  end
+end
+
+#asking user to select a block
+#return the array [r, c] of user's selection
+def ask_player_choose()
+  #user enter the row
+  say "plsese select the row (enter 1, 2, or 3)?"
+  r = gets.chomp.strip
+  while r != '1' && r != '2' && r != '3'
+    say "Note: invalided number! please enter 1, 2, or 3"
+    r = gets.chomp.strip
+  end
+
+  #user enter the col
+  say "plsese select the column (enter 1, 2, or 3)?"
+  c = gets.chomp.strip
+  while c != '1' && c != '2' && c != '3'
+    say "Note: invalided number! please enter 1, 2, or 3"
+    c = gets.chomp.strip
+  end
+  say "Your selection is [#{r}, #{c}]"
+  player_s = [(r.to_i - 1), (c.to_i - 1)]
+end
+
+
+#validate the selected block [r, c] is not selected in board_arrs
+def player_choose(board_arr)
+  valid_selection = false
+  begin
+    player_s = ask_player_choose
+    if board_arr[block_loc_convert(player_s)] == NO_ONE_S
+      board_arr[block_loc_convert(player_s)] = PLAY_S
+      valid_selection = true
+      board_arr #return the new board_arr
+    else
+      say "Note: the block has been selected! please select another block!"
+    end
+  end while !valid_selection
+end
+
+#computer randomly chooses a block on an given board
+def com_choose(board_arr)
+  valid_selection = false
+  say "Computer's turn:"
+  begin
+    com_s = [rand(3), rand(3)] #randomly select a block
+    if board_arr[block_loc_convert(com_s)] == NO_ONE_S
+      board_arr[block_loc_convert(com_s)] = COM_S
+      valid_selection = true
+      board_arr #return the new board_arr
+#binding.pry
+    end
+  end while !valid_selection
+end
+
+#ruturn the winning status below of the game
+#NO_ONE_WIN = 0 #no one wins the game
+#PLAYER_WIN = 1 #the play wins
+#COM_WIN = 2 #the computer wins
+#TIE_GAME = 3 #no one wins and all blocks are taken!
+def win_check(board_arr)
+  return_value = NO_ONE_WIN
+  #8 cases for winning status
+  c1 = board_arr[0] + board_arr[1] + board_arr[2]
+  c2 = board_arr[3] + board_arr[4] + board_arr[5]
+  c3 = board_arr[6] + board_arr[7] + board_arr[8]
+  c4 = board_arr[0] + board_arr[3] + board_arr[6]
+  c5 = board_arr[1] + board_arr[4] + board_arr[7]
+  c6 = board_arr[2] + board_arr[5] + board_arr[8]
+  c7 = board_arr[0] + board_arr[4] + board_arr[8]
+  c8 = board_arr[2] + board_arr[4] + board_arr[6]
+  #different cases return different values
+  status = [c1, c2, c3, c4, c5, c6, c7, c8]
+  status.each do |v| 
+    if v == 3 || v == -3
+      v == 3 ? return_value = PLAYER_WIN : return_value = COM_WIN  
+    end
+  end
+  #check tie game
+  if return_value == NO_ONE_WIN && !board_arr.include?(NO_ONE_S)
+    return_value = TIE_GAME
+  end
+  return_value
+end
+
+
+def draw_board(board_arr)
+  #transfer board into 'O' and 'X'
+  board_ox = board_arr.map do |v|
+    case 
+    when v == NO_ONE_S
+      "   "
+    when v == PLAY_S
+      " O "
+    when v == COM_S
+      " X "
+    end
+  end
+  #drawing the board now
+  puts "#{board_ox[0]} | #{board_ox[1]} | #{board_ox[2]}}"
+  puts "--------------"
+  puts "#{board_ox[3]} | #{board_ox[4]} | #{board_ox[5]}}"
+  puts "--------------"
+  puts "#{board_ox[6]} | #{board_ox[7]} | #{board_ox[8]}}"
+end
+
+#prints out game result and return final game status
+def game_result(player_name, game_status, board_arr)
+  game_status = win_check(board_arr)
+  case game_status
+  when PLAYER_WIN
+    say "Congraduates, #{player_name}. You win!"
+  when COM_WIN
+    say "Hoops! You lose!"
+  when TIE_GAME
+    say "It's a tie.  No one wins!"      
+  end
+  game_status  
+end
+
+
+def new_game()
+  #create a new board
+  board = new_board
+
+  #asking user's name
+  #Asking the player's name
+  say "Welcome to our Tic Tac Toe Game!"
+  say "What's your name?"
+  player_name = gets.chomp.strip
+
+  say "Hi #{player_name}, Game Starts!"
+
+  game_status = NO_ONE_WIN
+
+  while game_status == NO_ONE_WIN
+    player_choose(board)
+    draw_board(board)
+    game_status = game_result(player_name, game_status, board)
+    if game_status == NO_ONE_WIN
+      com_choose(board)
+      draw_board(board)
+      game_status = game_result(player_name, game_status, board)    
+    end
+  end
+end
+
+#main method
+new_game
+
+
+
+
+
+ 
